0725e59cc4041d2b85116ece27f7fbf7fdc4eca9
Matheus Oliveira de Carvalho <matheus.carvalho.corp@gmail.com>
2024-02-18 09:03:12 -0300

    Fix: Modificada a program pra chamar a classe com os menus interativos para diversas operacoes

--- bank-transfer-app/Program.cs
+++ bank-transfer-app/Program.cs
@@ -2,18 +2,22 @@
 
 using bank_transfer_app.Model;
 
-Conta minhaConta = new Conta(TipoConta.PessoaFisica, "Matheus", 2000, 0);
-Conta outraConta = new(TipoConta.PessoaFisica, "Julio", 0, 0);
+// Conta minhaConta = new Conta(TipoConta.PessoaFisica, "Matheus", 2000, 0);
+// Conta outraConta = new(TipoConta.PessoaFisica, "Julio", 0, 0);
 
-System.Console.WriteLine(minhaConta);
-System.Console.WriteLine(outraConta);
+// System.Console.WriteLine(minhaConta);
+// System.Console.WriteLine(outraConta);
 
-minhaConta.Sacar(200);
-minhaConta.Transferir(1200, outraConta);
-outraConta.Deposito(300);
-
-
+// minhaConta.Sacar(200);
+// minhaConta.Transferir(1200, outraConta);
+// outraConta.Deposito(300);
 
 
-System.Console.WriteLine(minhaConta);
-System.Console.WriteLine(outraConta);
+
+
+// System.Console.WriteLine(minhaConta);
+// System.Console.WriteLine(outraConta);
+
+Menu menu = new();
+
+menu.Acoes();
